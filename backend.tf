9a838ee0a6ce71ad288f4855947916aee4452af3
Vennila <nilarajan@gmail.com>
2024-06-25 22:11:25 +0800

    terraform code

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce6-tfstate"
+    key    = "vennila-ec2.tfstate" #Remember to change this
+    region = "ap-southeast-1"      #The region of your backend bucket
+  }
+}
+
+
